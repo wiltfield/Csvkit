fc538315c67975a00d202a1bacc2d5abdb707387
wiltfield <wiltfields@gmail.com>
2026-09-10 17:30:35 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { resolve } from 'path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base: process.env.GH_PAGES ? '/csvkit-plus/' : '/',
   build: {
     rollupOptions: {
       input: {
